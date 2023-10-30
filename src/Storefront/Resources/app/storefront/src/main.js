0f3cb2d91a70e387f37fe1d65be82c13f6a8016a
Frederik Rommel <frederik.rommel@webidea24.de>
2023-10-30 15:29:32 +0100

    TILSW6-20: create facility: add loading screen

--- src/Storefront/Resources/app/storefront/src/main.js
+++ src/Storefront/Resources/app/storefront/src/main.js
@@ -1,1 +1,8 @@
-// empty
+import TiltaCreateFacilityForm from './tilta-create-facility-form';
+
+const PluginManager = window.PluginManager;
+let pluginList = PluginManager.getPluginList();
+
+if (!('TiltaCreateFacilityForm' in pluginList)) {
+    PluginManager.register('TiltaCreateFacilityForm', TiltaCreateFacilityForm, '[data-tilta-create-facility-form="true"]');
+}
