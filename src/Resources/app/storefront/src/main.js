58136574ca54ed5aac481ae73af44c7aaa1eaab2
Frederik Rommel <frederik.rommel@webidea24.de>
2023-11-06 14:44:29 +0100

    TILSW6-23: fix bug, that theme got not compiled on de-/activate

---
+++ src/Resources/app/storefront/src/main.js
@@ -0,0 +1,4 @@
+// TILSW6-23: bugfix: this empty file is required to tell shopware it should recompile the theme on activate/deactivate.
+// last check of bug: 2023-11-06 (SW Version 6.5.6.1)
+
+// please keep this file empty, and add your style/script to the Storefront sub-bundle.
