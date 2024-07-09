c865ad5645490813abdff8cd823667397f4170b7
Frederik Rommel <frederik.rommel@webidea24.de>
2024-07-09 15:37:33 +0200

    TILSW6-33: add technical name (+rework payment-method bootstrap)

--- src/Core/Bootstrap/PaymentMethods.php
+++ src/Core/Bootstrap/PaymentMethods.php
@@ -10,11 +10,16 @@ declare(strict_types=1);
 
 namespace Tilta\TiltaPaymentSW6\Core\Bootstrap;
 
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Checkout\Payment\PaymentMethodCollection;
+use Shopware\Core\Checkout\Payment\PaymentMethodDefinition;
 use Shopware\Core\Checkout\Payment\PaymentMethodEntity;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Field;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\StorageAware;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\Uuid\Uuid;
 use Tilta\TiltaPaymentSW6\Core\PaymentHandler\TiltaDefaultPaymentHandler;
 
 class PaymentMethods extends AbstractBootstrap
@@ -25,6 +30,7 @@ class PaymentMethods extends AbstractBootstrap
             'name' => 'Tilta Invoice',
             'description' => 'Invoice Payment allows you to delay payment for your order until a more convenient time.',
             'afterOrderEnabled' => true,
+            'technicalName' => 'tilta_invoice',
             'translations' => [
                 'de-DE' => [
                     'name' => 'Rechnungskauf',
@@ -39,32 +45,42 @@ class PaymentMethods extends AbstractBootstrap
     ];
 
     /**
-     * @var EntityRepository<EntityCollection<PaymentMethodEntity>>
+     * @var EntityRepository<PaymentMethodCollection>
      */
-    private object $paymentRepository;
+    private EntityRepository $paymentRepository;
+
+    private PaymentMethodDefinition $paymentMethodDefinition;
+
+    private Connection $connection;
 
     public function injectServices(): void
     {
         /** @phpstan-ignore-next-line */
         $this->paymentRepository = $this->container->get('payment_method.repository');
+        /** @phpstan-ignore-next-line */
+        $this->paymentMethodDefinition = $this->container->get(PaymentMethodDefinition::class);
+        /** @phpstan-ignore-next-line */
+        $this->connection = $this->container->get(Connection::class);
     }
 
     public function update(): void
     {
-        foreach (self::PAYMENT_METHODS as $paymentMethod) {
-            $this->upsertPaymentMethod($paymentMethod);
-        }
+    }
 
-        // Keep active flags as they are
+    public function postUpdate(): void
+    {
+        $this->addPaymentMethods();
+        $this->setTechnicalNames();
+    }
+
+    public function postInstall(): void
+    {
+        $this->addPaymentMethods();
+        $this->setActiveFlags(false);
     }
 
     public function install(): void
     {
-        foreach (self::PAYMENT_METHODS as $paymentMethod) {
-            $this->upsertPaymentMethod($paymentMethod);
-        }
-
-        $this->setActiveFlags(false);
     }
 
     public function uninstall(bool $keepUserData = false): void
@@ -82,33 +98,12 @@ class PaymentMethods extends AbstractBootstrap
         $this->setActiveFlags(false);
     }
 
-    protected function upsertPaymentMethod(array $paymentMethod): void
-    {
-        $paymentSearchResult = $this->paymentRepository->search(
-            (
-                (new Criteria())
-                    ->addFilter(new EqualsFilter('handlerIdentifier', $paymentMethod['handlerIdentifier']))
-                    ->setLimit(1)
-            ),
-            $this->context
-        );
-
-        /** @var PaymentMethodEntity|null $paymentEntity */
-        $paymentEntity = $paymentSearchResult->first();
-        if ($paymentEntity instanceof PaymentMethodEntity) {
-            $paymentMethod['id'] = $paymentEntity->getId();
-        }
-
-        $paymentMethod['pluginId'] = $this->plugin->getId();
-        $this->paymentRepository->upsert([$paymentMethod], $this->context);
-    }
-
-    protected function setActiveFlags(bool $activated): void
+    private function setActiveFlags(bool $activated): void
     {
         /** @var PaymentMethodEntity[] $paymentEntities */
         $paymentEntities = $this->paymentRepository->search(
             (new Criteria())->addFilter(new EqualsFilter('pluginId', $this->plugin->getId())),
-            $this->context
+            $this->installContext->getContext()
         )->getElements();
 
         $updateData = array_map(static fn (PaymentMethodEntity $entity): array => [
@@ -116,6 +111,72 @@ class PaymentMethods extends AbstractBootstrap
             'active' => $activated,
         ], $paymentEntities);
 
-        $this->paymentRepository->update(array_values($updateData), $this->context);
+        $this->paymentRepository->update(array_values($updateData), $this->installContext->getContext());
+    }
+
+    private function addPaymentMethods(): void
+    {
+        // add payment methods which does not exist yet
+        $upsertData = [];
+        foreach (self::PAYMENT_METHODS as $paymentMethod) {
+            $criteria = new Criteria();
+            $criteria->addFilter(new EqualsFilter('handlerIdentifier', $paymentMethod['handlerIdentifier']));
+            $criteria->setLimit(1);
+
+            $id = $this->paymentRepository->searchIds($criteria, $this->installContext->getContext())->firstId();
+
+            if ($id === null) {
+                if (!$this->isFieldTechnicalNameAvailable()) {
+                    unset($paymentMethod['technicalName']);
+                }
+
+                $paymentMethod['pluginId'] = $this->plugin->getId();
+                $paymentMethod['active'] = false;
+                $upsertData[] = $paymentMethod;
+            }
+        }
+
+        if ($upsertData !== []) {
+            $this->paymentRepository->upsert($upsertData, $this->installContext->getContext());
+        }
+    }
+
+    private function setTechnicalNames(): void
+    {
+        if (!$this->isFieldTechnicalNameAvailable()) {
+            return;
+        }
+
+        $storageNames = [
+            'technicalName' => $this->getStorageName('technicalName'),
+            'handlerIdentifier' => $this->getStorageName('handlerIdentifier'),
+            'pluginId' => $this->getStorageName('pluginId'),
+        ];
+
+        foreach (self::PAYMENT_METHODS as $paymentMethod) {
+            $this->connection->update(
+                PaymentMethodDefinition::ENTITY_NAME,
+                [
+                    $storageNames['technicalName'] => $paymentMethod['technicalName'],
+                ],
+                [
+                    $storageNames['handlerIdentifier'] => $paymentMethod['handlerIdentifier'],
+                    $storageNames['pluginId'] => Uuid::fromHexToBytes($this->plugin->getId()),
+                    $storageNames['technicalName'] => null,
+                ]
+            );
+        }
+    }
+
+    private function isFieldTechnicalNameAvailable(): bool
+    {
+        return $this->paymentMethodDefinition->getField('technicalName') instanceof Field;
+    }
+
+    private function getStorageName(string $propertyName): string
+    {
+        $field = $this->paymentMethodDefinition->getField($propertyName);
+
+        return $field instanceof StorageAware ? $field->getStorageName() : $propertyName;
     }
 }
