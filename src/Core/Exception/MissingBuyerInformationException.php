3d81fe44d3a5af121abea0aa73f99522c651cc43
Frederik Rommel <frederik.rommel@webidea24.de>
2024-07-09 09:15:13 +0200

    TILSW6-33: resolve todo: parameters in MissingBuyerInformationException

--- src/Core/Exception/MissingBuyerInformationException.php
+++ src/Core/Exception/MissingBuyerInformationException.php
@@ -14,27 +14,16 @@ use Shopware\Core\Framework\ShopwareHttpException;
 
 class MissingBuyerInformationException extends ShopwareHttpException
 {
-    // deprecated: ShopwareHttpException::parameters has been added in 6.4.15 - can be removed for 6.5 (or >=6.4.15)
-    private array $_parameters = [];
-
     public function __construct(array $messages)
     {
-        $parameters = [
+        parent::__construct('There are missing fields', [
             'errors' => $messages,
-        ];
-        // deprecated: ShopwareHttpException::parameters has been added in 6.4.15 - can be removed for 6.5 (or >=6.4.15)
-        if (property_exists($this, 'parameters')) {
-            parent::__construct('There are missing fields', $parameters);
-        } else {
-            parent::__construct('There are missing fields');
-            $this->_parameters = $parameters;
-        }
+        ]);
     }
 
     public function getErrorMessages(): array
     {
-        // deprecated: ShopwareHttpException::parameters has been added in 6.4.15 - can be adjusted for 6.5 (or >=6.4.15)
-        $errors = method_exists($this, 'getParameter') ? $this->getParameter('errors') : $this->_parameters['errors'] ?? [];
+        $errors = $this->getParameter('errors');
 
         return is_array($errors) ? $errors : [];
     }
